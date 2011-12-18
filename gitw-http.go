c7c99bb732d86ec7c80103fe955a266c81cfbc81
Luka Napotnik <luka.napotnik@gmail.com>
2011-12-18 18:10:53 +0100

    first commit

---
+++ gitw-http.go
@@ -0,0 +1,188 @@
+/*
+ * gitw-http.go - simple utility that shows gitw projects and the checkout,
+ *                build and test statuses
+ *
+ * version 0.1
+ *
+ * Copyright (c) 2011, Luka Napotnik <luka.napotnik@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the <organization> nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+ 
+package main
+
+import (
+	"io"
+	"net/http"
+	"log"
+	"encoding/gob"
+	"strings"
+	"encoding/json"
+	"fmt"
+	"os"
+	"io/ioutil"
+)
+
+type Repository struct {
+	Name string
+	Location string
+	Build string
+	OutputDirectory string
+	NotifyEmail string
+}
+
+func LoadRepository(configFile string) Repository {
+
+	gob.Register(&Repository{})
+	r := strings.NewReader(configFile)
+	decoder := json.NewDecoder(r)
+
+	repo := Repository{}
+	e := decoder.Decode(&repo)
+	if e != nil {
+		fmt.Printf("Error decoding repository configuration from file: %s\n", e)
+	}
+	return repo
+}
+
+func LoadRepositories(configDir string) []Repository {
+	repositories := make([]Repository, 0)
+
+	if configDir[0] != '/' { // have relative path
+		cwd, _ := os.Getwd()
+		configDir = cwd + "/" + configDir
+	}
+	fmt.Printf(":: Reading repositories from '%s' ...\n", configDir)
+	ctx, err := ioutil.ReadDir(configDir)
+	if err != nil {
+		fmt.Printf("Error reading configuration directory: %s\n", err)
+		return nil
+	}
+	for _, file := range ctx {
+		if file.Mode().IsDir() {
+			continue
+		}
+		if strings.HasSuffix(file.Name(), ".json") == false {
+			continue
+		}
+		fileCtx, err := ioutil.ReadFile(configDir + "/" + file.Name())
+		if err != nil {
+			fmt.Printf("Error reading repository configuration from '%s': %s\n", file.Name(), err)
+			continue
+		}
+		repository := LoadRepository(string(fileCtx))
+		repositories = append(repositories, repository)
+	}
+	return repositories
+}
+
+
+func AnalyzeCheckut(repository Repository) bool {
+	out, err := ioutil.ReadFile(repository.Name + "-checkout-output.txt")
+	if err != nil {
+		return false
+	}
+	if strings.Index(string(out), "succ") == 0 {
+		return true
+	}
+	return false
+}
+
+func AnalyzeBuild(repository Repository) bool {
+	out, err := ioutil.ReadFile(repository.Name + "-buildt-output.txt")
+	if err != nil {
+		return false
+	}
+	if strings.Index(string(out), "succ") == 0 {
+		return true
+	}
+	return false
+}
+
+func AnalyzeTest(repository Repository) bool {
+	return false
+}
+
+func ViewLog(w http.ResponseWriter, req *http.Request) {
+	index := req.URL.Path[1:]
+	io.WriteString(w, index)
+}
+
+func Index(w http.ResponseWriter, req *http.Request) {
+index := `<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html><head><meta content="text/html; charset=UTF-8" http-equiv="content-type"><title>gitw [repository status]</title></head><body>
+<h1>Repository Status</h1>
+<table style="text-align: left; width: 639px; height: 54px;" border="1" cellpadding="2" cellspacing="2">
+  <tbody>
+    <tr>
+      <td style="vertical-align: top; font-weight: bold; width: 200px;">Name<br>
+      </td>
+      <td style="vertical-align: top; font-weight: bold; width: 100px;">Checkout<br>
+      </td>
+      <td style="vertical-align: top; font-weight: bold; width: 100px;">Build<br>
+      </td>
+      <td style="vertical-align: top; font-weight: bold; width: 100px;">Test<br>
+      </td>
+    </tr>
+`
+	repositories := LoadRepositories("repositories")
+	for _, repository := range repositories {
+
+		checkoutOutputFile := repository.Name + "-checkout-output.txt"
+		buildOutputFile := repository.Name + "-build-output.txt"
+
+		index += "<tr><td>" + repository.Name + "</td>"
+		status := AnalyzeCheckut(repository)
+		if status == false {
+			index += "<td><a href=\"viewlog/" + checkoutOutputFile + "\">failed</a></td>"
+		} else {
+			index += "<td><a href=\"viewlog/" + buildOutputFile + "\">success</a></td>"
+		}
+		status = AnalyzeBuild(repository)
+		if status == false {
+			index += "<td><a href=\"viewlog/" + checkoutOutputFile + "\">failed</a></td>"
+		} else {
+			index += "<td><a href=\"viewlog/" + buildOutputFile + "\">success</a></td>"
+		}
+		status = AnalyzeTest(repository)
+		if status == false {
+			index += "<td><a href=\"viewlog/" + checkoutOutputFile + "\">failed</a></td>"
+		} else {
+			index += "<td><a href=\"viewlog/" + buildOutputFile + "\">success</a></td>"
+		}
+		index += "</tr>"
+	}
+	index += `</tbody></table><br><hr style="width: 100%; height: 2px;">Powered by gitw<br></body></html>`
+	io.WriteString(w, index)
+}
+
+func main() {
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/viewlog/*", ViewLog)
+
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
---
+++ gitw.go
@@ -0,0 +1,197 @@
+/*
+ * gitw.go - simple utility that watches over local GIT repositores
+ *           and if a commit is detected, checks out the sources and
+ *           builds the project.
+ *
+ * version 0.1
+ *
+ * Copyright (c) 2011, Luka Napotnik <luka.napotnik@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the <organization> nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+package main
+
+import (
+	"encoding/json"
+	"os"
+	"os/exec"
+	"io/ioutil"
+	"fmt"
+	"encoding/gob"
+	"strings"
+	"exp/inotify"
+)
+
+type Repository struct {
+	Name string
+	Location string
+	Build string
+	OutputDirectory string
+	NotifyEmail string
+}
+
+func (repository Repository) Checkout() string {
+	status := []byte("succ\n\n")
+	tmpDir, _ := ioutil.TempDir(os.TempDir(), ".gitw-checkout-")
+	fmt.Printf(":: Checking out '%s' in %s\n", repository.Name, tmpDir)
+
+	cmd := exec.Command("git", "clone", repository.Location, tmpDir)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("Erorr running git: %s\n", err)
+		status = []byte("fail\n\n")
+	}
+	status = append(status, output...)
+	err = ioutil.WriteFile(repository.OutputDirectory + repository.Name + "-checkout-output.txt", status, 0644)
+	if err != nil {
+		fmt.Printf("Error writing checkout output to file: %s\n", err)
+	}
+	return tmpDir
+}
+
+func (repository Repository) BuildCheckout(tmpDir string) {
+	fmt.Printf(":: Building '%s'\n", repository.Name)
+	status := []byte("succ\n\n")
+	cmd := exec.Command(repository.Build)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("Erorr running %s: %s\n", repository.Build, err)
+		status = []byte("fail\n\n")
+	}
+	status = append(status, output...)
+
+	err = ioutil.WriteFile(repository.OutputDirectory + repository.Name + "-build-output.txt", status, 0644)
+	if err != nil {
+		fmt.Printf("Error writing checkout output to file: %s\n", err)
+	}
+}
+
+func LoadRepository(configFile string) Repository {
+
+	gob.Register(&Repository{})
+	r := strings.NewReader(configFile)
+	decoder := json.NewDecoder(r)
+
+	repo := Repository{}
+	e := decoder.Decode(&repo)
+	if e != nil {
+		fmt.Printf("Error decoding repository configuration from file: %s\n", e)
+	}
+	return repo
+}
+
+func LoadRepositories(configDir string) []Repository {
+	repositories := make([]Repository, 0)
+
+	if configDir[0] != '/' { // have relative path
+		cwd, _ := os.Getwd()
+		configDir = cwd + "/" + configDir
+	}
+	fmt.Printf(":: Reading repositories from '%s' ...\n", configDir)
+	ctx, err := ioutil.ReadDir(configDir)
+	if err != nil {
+		fmt.Printf("Error reading configuration directory: %s\n", err)
+		return nil
+	}
+	for _, file := range ctx {
+		if file.Mode().IsDir() {
+			continue
+		}
+		if strings.HasSuffix(file.Name(), ".json") == false {
+			continue
+		}
+		fileCtx, err := ioutil.ReadFile(configDir + "/" + file.Name())
+		if err != nil {
+			fmt.Printf("Error reading repository configuration from '%s': %s\n", file.Name(), err)
+			continue
+		}
+		repository := LoadRepository(string(fileCtx))
+		repositories = append(repositories, repository)
+	}
+	return repositories
+}
+
+
+func TestLoadRepository() bool {
+sampleConfig := `
+{
+	"name"		: "testing",
+	"location"	: "/home/luka/git/tarock.git",
+	"build"		: "gomake",
+	"outputdirectory" : "/home/luka/gitw/",
+	"notifyemail" : "joe@example.com"
+}
+	`
+	expectedRepository := Repository{"testing", "/home/luka/git/tarock.git", "gomake", "/home/luka/gitw/", "joe@example.com"}
+	repository := LoadRepository(sampleConfig)
+	if repository != expectedRepository {
+		return false
+	}
+	return true
+}
+
+func RepositoryWatchdog(repositories []Repository) {
+	watcher, err := inotify.NewWatcher()
+	gitWatchFile := "/refs/heads/master"
+	if err != nil {
+		fmt.Printf("Error creating new inotify watcher: %s\n", err)
+		return
+	}
+	for _, repository := range repositories {
+		fmt.Printf(":: Adding repository '%s' to watchlist\n", repository.Name)
+		err = watcher.AddWatch(repository.Location + gitWatchFile, inotify.IN_MODIFY)
+		if err != nil {
+			fmt.Printf("Error watching '%s': %s\n", repository.Location, err)
+			return
+		}
+	}
+
+	for {
+		select {
+			case ev := <-watcher.Event:
+				for _, repository := range repositories {
+					skipIdx := strings.Index(ev.Name, gitWatchFile)
+					if repository.Location == ev.Name[:skipIdx] {
+						tmpDir := repository.Checkout()
+						repository.BuildCheckout(tmpDir)
+						break
+					}
+				}
+			case err := <-watcher.Error:
+				fmt.Printf("Watch error: %s\n", err)
+		}
+	}
+}
+func main() {
+	// Some self-tests
+	if TestLoadRepository() == false {
+		fmt.Println("TestLoadRepository failed!")
+	}
+	// Load repository information form JSON configuration files
+	repositories := LoadRepositories("repositories")
+	// Run the watchdog loop
+	RepositoryWatchdog(repositories)
+}
